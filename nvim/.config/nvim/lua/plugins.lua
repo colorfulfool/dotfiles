3d2c1c65d92fc08e7262992fcd2e2e52b64f8db5
Anton Khamets <anton.khamets@hey.com>
2024-10-22 19:05:41 +0500

    Change binding for code actions

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -96,7 +96,7 @@ return packer.startup(function(use)
 	use {
 		"aznhe21/actions-preview.nvim",
 		config = function()
-			vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+			vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions)
 		end,
 	}
 
