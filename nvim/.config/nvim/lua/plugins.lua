867e1b78a7c7bdbed21f7b8fcb42dc122668ee9e
Anton Khamets <anton.khamets@hey.com>
2025-08-30 05:02:01 +0500

    Upgrade telescope

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -43,7 +43,8 @@ return packer.startup(function(use)
   use("AstroNvim/astrotheme")
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    'nvim-telescope/telescope.nvim',
+    commit = '814f102',
     requires = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-telescope/telescope-github.nvim' },
