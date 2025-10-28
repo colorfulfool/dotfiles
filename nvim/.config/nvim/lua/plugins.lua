5546edc2a5200f4c651ec94656863c6c0504a5c8
Anton Khamets <anton.khamets@hey.com>
2025-10-28 23:52:25 +0500

    Fix i18n-menu binding

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -319,7 +319,7 @@ return packer.startup(function(use)
     },
     config = function()
       require("i18n-menu").setup()
-      vim.keymap.set("n", "<leader>ii", ":TranslateMenucr")
+      vim.keymap.set("n", "<leader>ii", ":TranslateMenu<CR>")
     end,
   }
 
