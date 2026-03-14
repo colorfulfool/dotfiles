2f7d66b9a3f2d7866c526e540bda7025486655a4
Anton Khamets <anton.khamets@hey.com>
2026-03-14 21:38:16 +0500

    Add dark/light wofi theme switching

--- .theme-script/go-dark.sh
+++ .theme-script/go-dark.sh
@@ -2,3 +2,4 @@
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeDark.colors|' ~/.config/qt6ct/qt6ct.conf
+ln -sf ~/.config/wofi/style-dark.css ~/.config/wofi/style.css
--- .theme-script/go-light.sh
+++ .theme-script/go-light.sh
@@ -2,3 +2,4 @@
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeLight.colors|' ~/.config/qt6ct/qt6ct.conf
+ln -sf ~/.config/wofi/style-light.css ~/.config/wofi/style.css
