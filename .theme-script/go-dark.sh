dac9a13744c3eb0f7f7892d0bea397c847d808c6
Anton Khamets <anton.khamets@hey.com>
2026-03-14 21:01:24 +0500

    Add Qt color scheme switching via qt6ct

--- .theme-script/go-dark.sh
+++ .theme-script/go-dark.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeDark.colors|' ~/.config/qt6ct/qt6ct.conf
--- .theme-script/go-light.sh
+++ .theme-script/go-light.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeLight.colors|' ~/.config/qt6ct/qt6ct.conf
