039848c46e391ee0221f39823f2e1c5ce9f2f1e3
Anton Khamets <anton.khamets@hey.com>
2026-03-17 20:56:12 +0500

    Set KDE theme with kwriteconfig instead of seding the file

--- .theme-script/go-dark.sh
+++ .theme-script/go-dark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeDark.colors|' ~/.config/qt6ct/qt6ct.conf
+kwriteconfig6 --file ~/.config/kdeglobals --group General --key ColorScheme BreezeDark
+rm -f ~/.config/gtk-4.0/gtk.css
--- .theme-script/go-light.sh
+++ .theme-script/go-light.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
-sed -i 's|color_scheme_path=.*|color_scheme_path=/usr/share/color-schemes/BreezeLight.colors|' ~/.config/qt6ct/qt6ct.conf
+kwriteconfig6 --file ~/.config/kdeglobals --group General --key ColorScheme BreezeLight
+rm -f ~/.config/gtk-4.0/gtk.css
