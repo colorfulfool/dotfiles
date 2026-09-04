e243e0fd69adce19c80a923bf48bd83556c489f6
Anton Khamets <anton.khamets@hey.com>
2026-09-04 13:08:28 +0500

    convert HJKL and S keybinds to evdev codes for layout independence

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -170,20 +170,20 @@ hl.bind("SUPER + ALT + left", hl.dsp.window.resize({ x = -100, y = 0 }))
 hl.bind("SUPER + ALT + up", hl.dsp.window.resize({ x = 0, y = -100 }))
 hl.bind("SUPER + ALT + down", hl.dsp.window.resize({ x = 0, y = 100 }))
 
-hl.bind("SUPER + H", hl.dsp.focus({ direction = "left" }))
-hl.bind("SUPER + L", hl.dsp.focus({ direction = "right" }))
-hl.bind("SUPER + K", hl.dsp.focus({ direction = "up" }))
-hl.bind("SUPER + J", hl.dsp.focus({ direction = "down" }))
+hl.bind("SUPER + code:35", hl.dsp.focus({ direction = "left" }))
+hl.bind("SUPER + code:38", hl.dsp.focus({ direction = "right" }))
+hl.bind("SUPER + code:37", hl.dsp.focus({ direction = "up" }))
+hl.bind("SUPER + code:36", hl.dsp.focus({ direction = "down" }))
 
-hl.bind("SUPER + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
-hl.bind("SUPER + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
-hl.bind("SUPER + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
-hl.bind("SUPER + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
+hl.bind("SUPER + SHIFT + code:35", hl.dsp.window.move({ direction = "left" }))
+hl.bind("SUPER + SHIFT + code:38", hl.dsp.window.move({ direction = "right" }))
+hl.bind("SUPER + SHIFT + code:37", hl.dsp.window.move({ direction = "up" }))
+hl.bind("SUPER + SHIFT + code:36", hl.dsp.window.move({ direction = "down" }))
 
-hl.bind("SUPER + ALT + H", hl.dsp.window.resize({ x = 100, y = 0 }))
-hl.bind("SUPER + ALT + L", hl.dsp.window.resize({ x = -100, y = 0 }))
-hl.bind("SUPER + ALT + K", hl.dsp.window.resize({ x = 0, y = -100 }))
-hl.bind("SUPER + ALT + J", hl.dsp.window.resize({ x = 0, y = 100 }))
+hl.bind("SUPER + ALT + code:35", hl.dsp.window.resize({ x = 100, y = 0 }))
+hl.bind("SUPER + ALT + code:38", hl.dsp.window.resize({ x = -100, y = 0 }))
+hl.bind("SUPER + ALT + code:37", hl.dsp.window.resize({ x = 0, y = -100 }))
+hl.bind("SUPER + ALT + code:36", hl.dsp.window.resize({ x = 0, y = 100 }))
 
 for i = 1, 10 do
     local key = i % 10
